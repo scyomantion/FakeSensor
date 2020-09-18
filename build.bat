a0ba980b77108ee4e8b057ce0cffc65c099172bc
Steven-Mike Duckerschein <steven.duckerschein@gmail.com>
2020-09-18 19:56:33 +0200

    use cmake as build tool. cleanup folder, increase version to v0.1.1

---
+++ build.bat
@@ -0,0 +1,8 @@
+cmake -A Win32 -S . -B "build32"
+cmake -A x64 -S . -B "build64"
+
+cmake --build build32 --config Release
+cmake --build build32 --config Release --target create_zip
+
+cmake --build build64 --config Release
+cmake --build build64 --config Release --target create_zip
